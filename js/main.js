abbe8d0aabff1ce32a505f4204fb7688663d259e
rdGoat <rdGoat@gmail.com>
2016-01-13 19:05:27 +1100

    add workshops

--- js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  $(".icon").click(function () {
-    $(".icon").toggleClass("icon-animate");
-    $(".mobilenav").fadeToggle(500);
-    $(".top-menu").toggleClass("top-animate");
-    $(".mid-menu").toggleClass("mid-animate");
-    $(".bottom-menu").toggleClass("bottom-animate");
-  });
-});
